13d7a7cfe3123616f6ed5854bbf627e03edc5dae
GameOfTrump <gameoftrump0407@gmail.com>
2024-06-28 11:08:33 +0200

    Delete TrumpMemeCoin directory

--- TrumpMemeCoin/js/app.js
+++
@@ -1,8 +0,0 @@
-const readFull = document.getElementById("readfull");
-const readBox = document.getElementById("historytextid");
-
-function toggleClass() {
-    readBox.classList.toggle("historytextlong");
-}
-
-readFull.addEventListener("click", toggleClass);
